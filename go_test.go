620ace1e3b0c601245e55bd3e1fab3fd519b5fed
jaysonesmith <github@nes.33mail.com>
2019-01-09 22:48:10 -0700

    Add `go test` file

---
+++ go_test.go
@@ -0,0 +1,33 @@
+package example
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/DATA-DOG/godog"
+	"github.com/DATA-DOG/godog/colors"
+)
+
+var opt = godog.Options{
+	Output: colors.Colored(os.Stdout),
+	Format: "pretty", // can define default values
+}
+
+func init() {
+	godog.BindFlags("godog.", flag.CommandLine, &opt)
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	opt.Paths = flag.Args()
+
+	status := godog.RunWithOptions("godogs", func(s *godog.Suite) {
+		FeatureContext(s)
+	}, opt)
+
+	if st := m.Run(); st > status {
+		status = st
+	}
+	os.Exit(status)
+}
